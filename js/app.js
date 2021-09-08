fd184422123f890b14a5228275f97a145ba68053
Fermín Fernández <40477091+fernandezfermin@users.noreply.github.com>
2021-09-08 11:16:35 -0400

    Commit

---
+++ js/app.js
@@ -0,0 +1,139 @@
+// Constructor para Seguro
+function Seguro(marca, anio, tipo) {
+     this.marca = marca;
+     this.anio = anio;
+     this.tipo = tipo;
+}
+Seguro.prototype.cotizarSeguro = function() {
+     /*
+          1 = american 1.15
+          2 = asiatic 1.05
+          3 = european 1.35
+     */
+     let cantidad;
+     const base = 100;
+
+     switch(this.marca){
+          case '1':
+               cantidad = base * 1.15;
+               break;
+          case '2':
+               cantidad = base * 1.05;
+               break;
+          case '3':
+               cantidad = base * 1.35;
+               break;
+     }
+
+     const diferencia = new Date().getFullYear() - this.anio;
+     cantidad -= ((diferencia * 3) * cantidad) / 100;
+
+    if(this.tipo === 'Basic') {
+         cantidad *= 1.30;
+    } else {
+         cantidad *= 1.50;
+    }
+
+     return cantidad;
+
+}
+
+function Interfaz() {}
+
+Interfaz.prototype.mostrarMensaje = function(mensaje, tipo) {
+     const div = document.createElement('div');
+
+     if(tipo === 'error') {
+          div.classList.add('mensaje','error');
+     } else {
+          div.classList.add('mensaje','correcto');
+     }
+     div.classList.add('mt-10');
+     div.innerHTML = `${mensaje}`;
+     formulario.insertBefore(div, document.querySelector('#resultado'));
+
+     setTimeout( () =>  {
+          document.querySelector('.mensaje').remove();
+     }, 3000);
+} 
+
+Interfaz.prototype.mostrarResultado = function(seguro, total) {
+     const resultado = document.querySelector('#resultado');
+     let marca;
+     switch(seguro.marca) {
+          case '1':
+               marca = 'American';
+               break;
+          case '2':
+               marca = 'Asiatic';
+               break;
+          case '3':
+               marca = 'European';
+               break;
+     }
+     const div = document.createElement('div');
+     div.classList.add('mt-10')
+     div.innerHTML = `
+          <p class='header'>Summary: </p>
+          <p class="font-bold">Brand: <span class="font-normal"> ${marca} </span> </p>
+          <p class="font-bold">Year: <span class="font-normal"> ${seguro.anio} </span> </p>
+          <p class="font-bold">Type: <span class="font-normal"> ${seguro.tipo} </span> </p>
+          <p class="font-bold"> Total: <span class="font-normal"> $ ${total} </span> </p>
+     `;
+
+     const spinner = document.querySelector('#cargando');
+     spinner.style.display = 'block';
+     setTimeout( () =>  {
+          spinner.style.display = 'none';
+          resultado.appendChild(div);
+     }, 3000);
+     
+}
+
+Interfaz.prototype.llenarOpciones = function () {
+     const max = new Date().getFullYear(),
+          min = max - 20;
+
+     const selectAnios = document.querySelector('#year');
+     for(let i = max; i > min; i--) {
+          let option = document.createElement('option');
+          option.value = i;
+          option.innerHTML = i;
+          selectAnios.appendChild(option);
+     }   
+}
+
+const interfaz = new Interfaz();
+
+document.addEventListener('DOMContentLoaded', () => {
+     interfaz.llenarOpciones()
+});
+
+const formulario = document.querySelector('#cotizar-seguro');
+
+formulario.addEventListener('submit', e =>  {
+     e.preventDefault();
+
+     const marca = document.querySelector('#marca').value;
+
+     const year = document.querySelector('#year').value
+
+     const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+ 
+
+     if(marca === '' || year === '' || tipo === '') {
+          interfaz.mostrarMensaje('Information is missing, please check and try again', 'error');
+     } else {
+          const resultados = document.querySelector('#resultado div');
+          if(resultados != null) {
+               resultados.remove();
+          }
+
+          const seguro = new Seguro(marca, year, tipo);
+          const cantidad = seguro.cotizarSeguro();
+          interfaz.mostrarResultado(seguro, cantidad);
+          interfaz.mostrarMensaje('Calculating...', 'exito');
+     }
+
+});
